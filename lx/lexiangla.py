dbd58c1244a955cfd743c25807519d3b04d873e2
aaron <775856746@qq.com>
2022-07-22 16:15:57 +0800

    增加知识库

--- lx/lexiangla.py
+++ lx/lexiangla.py
@@ -19,7 +19,7 @@ headers = {
 list = []
 
 
-def yjsl():
+def k8_yjsl():
     k8_resp = requests.get('https://lexiangla.com/gapi/v1/teams?limit=30&page=1&filter=list', headers=headers)
     if k8_resp.status_code == 200:
         for k8 in k8_resp.json()['data']:
@@ -32,36 +32,64 @@ def yjsl():
                         'https://lexiangla.com/api/v1/teams/' + k8['code'] + '/docs/' + doc[
                             'id'] + '?lazy_load=1&increment=1',
                         headers=headers)
-                    doc_detail_resp = doc_detail.json()
-                    if not (doc_detail_resp['target']['is_favorited'] and doc_detail_resp['target']['is_liked']):
-                        headers['x-xsrf-token'] = urllib.parse.unquote(
-                            re.search('XSRF-TOKEN=(.*?);', doc_detail.headers['set-cookie']).group(1))
-
-                        # 点赞
-                        print(requests.put(
-                            'https://lexiangla.com/api/v1/staff/likes/documents/' + doc_detail_resp['target_id'],
-                            headers=headers).status_code)
-                        # 收藏
-                        print(requests.put(
-                            'https://lexiangla.com/api/v1/staff/favorites/documents/' + doc_detail_resp['target_id'],
-                            headers=headers).status_code)
-                        # 评论
-                        payload = {
-                            "target_id": doc_detail_resp['target_id'],
-                            'target_type': 'document',
-                            'content': '/强'
-                        }
-                        requests.post("https://lexiangla.com/api/v1/comments", data=payload, headers=headers)
-                        list.append(doc_detail_resp['name'])
-        send_bark('任务成功', '任务执行完毕 ^_^' + '\n' + '\n'.join(list))
+                    sl(doc_detail)
     else:
         print("未登录")
-        send_bark('登陆信息失效', '登陆信息失效, 请重新登陆~')
+        send_bark('登陆信息失效', '[K8]:登陆信息失效, 请重新登陆~')
 
 
 def send_bark(title, content):
     requests.get("https://api.day.app/" + os.environ['BARK_KEY'] + '/' + title + '/' + content)
 
 
+def doc_yjsl():
+    # 获取全部doc
+    doc_resp = requests.get('https://lexiangla.com/api/v1/docs?filter=category&limit=20&page=1&order=-created_at'
+                            '&category_id=', headers=headers)
+    if doc_resp.status_code == 200:
+        for doc in doc_resp.json()['data']:
+            doc_detail = requests.get(
+                'https://lexiangla.com/api/v1/docs/' + doc['id'] + '?lazy_load=1&increment=1',
+                headers=headers)
+            # 如果点赞和收藏过中断执行
+            detail_json = doc_detail.json()
+            if detail_json['target']['is_favorited'] and detail_json['target']['is_liked']:
+                print(detail_json['name'])
+                return
+            sl(doc_detail)
+        if not list:
+            send_bark('任务成功', '任务执行完毕 ^_^' + '\n' + '\n'.join(list))
+        else:
+            send_bark('任务成功', '任务执行完毕 ^_^! 没有需要点赞的文章')
+    else:
+        print("未登录")
+        send_bark('登陆信息失效', '[知识库]:登陆信息失效, 请重新登陆~')
+
+
+def sl(doc_detail):
+    doc_detail_resp = doc_detail.json()
+    if not (doc_detail_resp['target']['is_favorited'] and doc_detail_resp['target']['is_liked']):
+        headers['x-xsrf-token'] = urllib.parse.unquote(
+            re.search('XSRF-TOKEN=(.*?);', doc_detail.headers['set-cookie']).group(1))
+
+        # 点赞
+        print(requests.put(
+            'https://lexiangla.com/api/v1/staff/likes/documents/' + doc_detail_resp['target_id'],
+            headers=headers).status_code)
+        # 收藏
+        print(requests.put(
+            'https://lexiangla.com/api/v1/staff/favorites/documents/' + doc_detail_resp['target_id'],
+            headers=headers).status_code)
+        # 评论
+        payload = {
+            "target_id": doc_detail_resp['target_id'],
+            'target_type': 'document',
+            'content': '/强'
+        }
+        requests.post("https://lexiangla.com/api/v1/comments", data=payload, headers=headers)
+        list.append(doc_detail_resp['name'])
+
+
 if __name__ == '__main__':
-    yjsl()
+    k8_yjsl()
+    doc_yjsl()
