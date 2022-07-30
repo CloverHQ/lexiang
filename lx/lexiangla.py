e869491a3c50ddddd22594755e71845da39e148f
mimikatz <775856746@qq.com>
2022-07-30 06:59:07 +0000

    vscode

--- lx/lexiangla.py
+++ lx/lexiangla.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Time    : 2022-07-21 16:54
-# @Author  : liuwenchao
+# @Author  : mimikatz
 # @File    : lexiangla
-# @Software: IntelliJ IDEA
+# @Software: vscode
 import json
 import os
 import random
