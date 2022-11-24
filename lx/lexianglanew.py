32072fb96725bf1b79d622dd7146ba86242e339e
clover <775856746@qq,com>
2022-11-24 18:13:53 +0800

    init

---
+++ lx/lexianglanew.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022-11-24 18:11
+# @Author  : liuwenchao
+# @File    : lexianglanew
+# @Software: IntelliJ IDEA
+
+
+def read():
+    pass
+
+
+def comment():
+    pass
+
+
+def like():
+    pass
+
+
+def journal_list():
+    pass
