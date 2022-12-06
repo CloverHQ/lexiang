f0c465b873559c1ec2a27759ff288bca0b09d8d7
clover <775856746@qq,com>
2022-12-06 12:16:41 +0800

    click

--- xlsx/click.py
+++ xlsx/click.py
@@ -1,7 +1,9 @@
-import os
-import pyautogui
+import sys
 import time
+
 import keyboard
+import pyautogui
+
 print('把鼠标移动到需要的位置，5秒后会获取坐标')
 print('Move the mouse to the desired position, and the coordinates will be obtained after 5 seconds')
 i = 1
@@ -13,25 +15,18 @@ print('按下空格停止')
 print('Press space to stop')
 while True:
     flag = False
+
+
     def key_press(key):
         global flag
         if key.name == 'space':
             flag = True
+
+
     keyboard.on_press(key_press)
     pyautogui.click(p)
     time.sleep(1)
-    if flag == True:
+    if flag:
         flag = False
         print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-        os._exit(0)
-#ChineseJiuran原创
-#ChineseJiuran原创
-#ChineseJiuran原创
-#ChineseJiuran原创
-#ChineseJiuran原创
-
-#ChineseJiuran original
-#ChineseJiuran original
-#ChineseJiuran original
-#ChineseJiuran original
-#ChineseJiuran original
+        sys.exit()
